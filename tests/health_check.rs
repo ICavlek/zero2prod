ca6714b8414b1274cf2a632646ba7b589a5a1f29
ICavlek <ivan.cavlek1403@gmail.com>
2023-09-27 21:52:44 +0200

    Added test that shows the POST form

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -20,3 +20,19 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute reques");
+    assert_eq!(200, response.status().as_u16());
+}
