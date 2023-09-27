60c2216b927013acf3de79095f0dfe8ab1d9097c
ICavlek <ivan.cavlek1403@gmail.com>
2023-09-27 22:26:22 +0200

    Printing server address and port value for curl testing

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,9 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    println!(
+        "🚀 Listening on http://127.0.0.1:{}",
+        listener.local_addr().unwrap().port()
+    );
     run(listener)?.await
 }
