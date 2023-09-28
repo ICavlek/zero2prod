30a0aefec97939263f87b84d91e0668ff3063b64
ICavlek <ivan.cavlek1403@gmail.com>
2023-09-28 16:10:22 +0200

    Binding app to fixed port for easier curl testing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     println!(
         "🚀 Listening on http://127.0.0.1:{}",
         listener.local_addr().unwrap().port()
