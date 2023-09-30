0b919fa13318bdea41f1c6c6eddf4175c52fac1d
ICavlek <ivan.cavlek1403@gmail.com>
2023-09-30 13:12:46 +0200

    Adding PgConnection to App as an Arc wrap via web::Data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -5,11 +6,14 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind configurationa address");
     println!(
         "🚀 Listening on http://127.0.0.1:{}",
         listener.local_addr().unwrap().port()
     );
-    run(listener)?.await
+    run(listener, connection)?.await
 }
