c9c2b4d54712ab93133949288d4f6c8bb4c7bc68
ICavlek <ivan.cavlek1403@gmail.com>
2023-10-03 14:01:32 +0200

    Added tracing into tests. Needs to be Lazy because it has to be initialized only once

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,14 @@ use uuid::Uuid;
 use zero2prod::{
     configuration::{get_configuration, DatabaseSettings},
     startup::run,
+    telemetry::{get_subscriber, init_subscriber},
 };
+use once_cell::sync::Lazy;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let subscriber = get_subscriber("test".into(), "debug".into());
+    init_subscriber(subscriber);
+});
 
 pub struct TestApp {
     pub address: String,
@@ -12,6 +19,7 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> (TestApp, DatabaseSettings) {
+    Lazy::force(&TRACING);
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
