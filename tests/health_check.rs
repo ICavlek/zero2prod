08532823a757b9c3c852681e7148d99ef0a4f40c
ICavlek <ivan.cavlek1403@gmail.com>
2023-10-03 15:14:35 +0200

    Wrapped sensitive stuff into Secret - Password, database url

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -50,14 +51,15 @@ async fn spawn_app() -> (TestApp, DatabaseSettings) {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
@@ -68,9 +70,10 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 }
 
 pub async fn drop_database(config: &DatabaseSettings) {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"DROP DATABASE "{}" WITH (FORCE);"#, config.database_name).as_str())
         .await
