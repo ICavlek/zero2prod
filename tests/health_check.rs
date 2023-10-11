c184828dbc0c3129932d658f2d843fb5016357e5
ICavlek <ivan.cavlek1403@gmail.com>
2023-10-11 17:13:10 +0200

    Updated test name and message

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -143,7 +143,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
     let (app, database_config) = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
@@ -164,7 +164,7 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
         assert_eq!(
             200,
             response.status().as_u16(),
-            "The API did not return a 200 OK when the payload was {}.",
+            "The API did not return a 400 Bad Request when the payload was {}.",
             description
         )
     }
