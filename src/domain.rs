264f8376add1c48978857e06d7d3e29b88100a19
ICavlek <ivan.cavlek1403@gmail.com>
2023-10-11 16:48:11 +0200

    Type drive development - We assert that any instance of SubscriberName will satisfy our validation constraints
    Impossible for an instance of SubscriberName to violate these constraints

---
+++ src/domain.rs
@@ -0,0 +1,43 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct NewSubscriber {
+    pub email: String,
+    pub name: SubscriberName,
+}
+
+pub struct SubscriberName(String);
+
+impl SubscriberName {
+    /// Returns an instance of `SubscriberName` if the input satisfies all our validation constraints
+    /// on subscriber names, It panics otherwise.
+    pub fn parse(s: String) -> SubscriberName {
+        let is_empty_or_whitespace = s.trim().is_empty();
+        let is_too_long = s.graphemes(true).count() > 256;
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            panic!("{} is not a valid subscriber name", s)
+        } else {
+            Self(s)
+        }
+    }
+
+    /// Expose it by value, consuming the struct itself
+    pub fn inner(self) -> String {
+        // The caller gets the inner string,
+        // but they do not have a SubscriberName anymore!
+        // That's because `inner` takes `self` by value,
+        // consuming it according to move semantics
+        self.0
+    }
+
+    /// Expose a mutable reference
+    pub fn inner_mut(&mut self) -> &mut str {
+        &mut self.0
+    }
+
+    /// Expose a shared reference
+    pub fn inner_ref(&self) -> &str {
+        &self.0
+    }
+}
