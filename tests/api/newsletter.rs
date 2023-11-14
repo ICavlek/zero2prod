d125d7c095c39e29ab6c80876525b5e05550fb30
ICavlek <ivan.cavlek1403@gmail.com>
2023-11-14 19:54:15 +0100

    Newsletter not delivered to unconfirmed subscriber - Test FAIL

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,11 +1,35 @@
-use wiremock::{Mock, matchers::{method, path}, ResponseTemplate};
+use wiremock::{Mock, matchers::{method, path, any}, ResponseTemplate};
 
-use crate::helpers::{spawn_app, TestApp};
+use crate::helpers::{spawn_app, TestApp, drop_database};
 
 #[tokio::test]
 async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     let app = spawn_app().await;
     create_unconfirmed_subscriber(&app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(0)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    drop_database(&app.db_settings).await;
+    assert_eq!(response.status().as_u16(), 200);
 }
 
 async fn create_unconfirmed_subscriber(app: &TestApp) {
