c885755bccc4af5dc86cdaea86312f31d0112d5c
ICavlek <ivan.cavlek1403@gmail.com>
2023-11-14 16:54:29 +0100

    Added chain of errors info to UnexpectedError

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -45,18 +45,31 @@ pub async fn subscribe(
     let mut transaction = pool
         .begin()
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to acquire a Postgres connection from the pool".into(),
+        ))?;
     let subscriber_id = insert_subscriber(&mut transaction, &new_subscriber)
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to insert new subscriber in the database.".into(),
+        ))?;
     let subscription_token = generate_subscription_token();
     store_token(&mut transaction, subscriber_id, &subscription_token)
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to store the confirmation token for a new \
+            subscriber.".into(),
+        ))?;
     transaction
         .commit()
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to commit SQL transaction to store a new subscriber.".into(),
+        ))?;
     send_confirmation_email(
         &email_client,
         new_subscriber,
@@ -64,7 +77,10 @@ pub async fn subscribe(
         &subscription_token,
     )
     .await
-    .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+    .map_err(|e| SubscribeError::UnexpectedError(
+        Box::new(e),
+        "Failed to send a confirmation email.".into(),
+    ))?;
 
     Ok(HttpResponse::Ok().finish())
 }
@@ -194,8 +210,8 @@ fn error_chain_fmt(
 pub enum SubscribeError {
     #[error("{0}")]
     ValidationError(String),
-    #[error(transparent)]
-    UnexpectedError(#[from] Box<dyn std::error::Error>),
+    #[error("{1}")]
+    UnexpectedError(#[source] Box<dyn std::error::Error>, String),
 }
 
 impl std::fmt::Debug for SubscribeError {
@@ -208,7 +224,7 @@ impl ResponseError for SubscribeError {
     fn status_code(&self) -> reqwest::StatusCode {
         match self {
             SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            SubscribeError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            SubscribeError::UnexpectedError(_, _) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
