8a2b879dd5a88bffdccaf7d8cfe4147305042138
ICavlek <ivan.cavlek1403@gmail.com>
2023-11-14 19:38:59 +0100

    Added unconfirmed subscriber

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,3 +1,5 @@
+use wiremock::{Mock, matchers::{method, path}, ResponseTemplate};
+
 use crate::helpers::{spawn_app, TestApp};
 
 #[tokio::test]
@@ -6,4 +8,19 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     create_unconfirmed_subscriber(&app).await;
 }
 
-async fn create_unconfirmed_subscriber(_app: &TestApp) {}
+async fn create_unconfirmed_subscriber(app: &TestApp) {
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .named("Create unconfirmed subscriber")
+        .expect(1)
+        .mount_as_scoped(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into())
+        .await
+        .error_for_status()
+        .unwrap();
+}
