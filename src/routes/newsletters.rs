2469f1b1760bccfd7c5567bceea6075f60429a62
ICavlek <ivan.cavlek1403@gmail.com>
2023-11-22 16:00:27 +0100

    Added struct skeleton for our newsletter

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,17 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
 
-pub async fn publish_newsletter() -> HttpResponse {
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    _title: String,
+    _content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    _html: String,
+    _text: String,
 }
