7a67b253a7b5c3bc8f630e8ff5cb19d805c428b7
ICavlek <ivan.cavlek1403@gmail.com>
2023-12-08 16:03:54 +0100

    Saving error reporting, frontend fail - Test SUCCESS

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,3 +1,4 @@
+use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::{web, HttpResponse};
 use hmac::{Hmac, Mac};
@@ -21,7 +22,7 @@ pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
     secret: web::Data<Secret<String>>,
-) -> HttpResponse {
+) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
         password: form.0.password,
@@ -30,9 +31,9 @@ pub async fn login(
     match validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("username", &tracing::field::display(&user_id));
-            HttpResponse::SeeOther()
+            Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/"))
-                .finish()
+                .finish())
         }
         Err(e) => {
             let e = match e {
@@ -47,12 +48,13 @@ pub async fn login(
                 mac.update(query_string.as_bytes());
                 mac.finalize().into_bytes()
             };
-            HttpResponse::SeeOther()
+            let response = HttpResponse::SeeOther()
                 .insert_header((
                     LOCATION,
                     format!("/login?{}&tag={:x}", query_string, hmac_tag),
                 ))
-                .finish()
+                .finish();
+            Err(InternalError::from_response(e, response))
         }
     }
 }
