968a4ffb0a6ad617c3d1d85d0eda9728d675185d
ICavlek <ivan.cavlek1403@gmail.com>
2023-12-09 20:10:26 +0100

    Extracting flash cookie in a proper manner - Test FAIL

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,7 +1,3 @@
-use std::collections::HashSet;
-
-use reqwest::header::HeaderValue;
-
 use crate::helpers::{assert_is_redirect_to, drop_database, spawn_app};
 
 #[tokio::test]
@@ -15,12 +11,6 @@ async fn an_error_flash_message_is_set_on_failure() {
     drop_database(&app.db_settings).await;
     assert_is_redirect_to(&response, "/login");
 
-    let cookies: HashSet<_> = response
-        .headers()
-        .get_all("Set-Cookie")
-        .into_iter()
-        .collect();
-    assert!(
-        cookies.contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap())
-    )
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
