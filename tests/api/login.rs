70d1b90f5598ed997b630541f5dad7d61cc25e8a
ICavlek <ivan.cavlek1403@gmail.com>
2023-12-10 19:01:40 +0100

    Added response of html after successful get username - Test SUCCESS

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -31,9 +31,9 @@ async fn redirect_to_admin_dashboard_after_login_success() {
     });
 
     let response = app.post_login(&login_body).await;
-    drop_database(&app.db_settings).await;
     assert_is_redirect_to(&response, "/admin/dashboard");
 
     let html_page = app.get_admin_dashboard_html().await;
     assert!(html_page.contains(&format!("Welcome {}", app.test_user.username)));
+    drop_database(&app.db_settings).await;
 }
